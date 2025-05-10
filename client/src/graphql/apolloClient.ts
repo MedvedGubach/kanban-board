ac21cfab98a3db19f976df90b6c8ef4ccd992d30
Emmanuel <emmanuel.arana.gutierrez@gmail.com>
2025-05-09 21:26:45 -0600

    fixes prod and local development, uri management

--- client/src/graphql/apolloClient.ts
+++ client/src/graphql/apolloClient.ts
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
-const httpLink = createHttpLink({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
-});
+const uri = import.meta.env.VITE_GRAPHQL_URI || process.env.NEXT_PUBLIC_GRAPHQL_URI;
+
+const httpLink = createHttpLink({ uri, });
 
 const authLink = setContext((_, { headers }) => {
     const token = sessionStorage.getItem("token");
